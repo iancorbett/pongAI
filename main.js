f5506df139b692e8f3bd7155d0115e0d0ee8f5c3
iancorbett <iancorbett1324@gmail.com>
2025-08-19 23:03:14 -0700

    fix typo on line 2 to get context correctly

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("game");
-const ctx = cvs.getContext("2d");
+const ctx = canvas.getContext("2d");
 const p1ScoreEl = document.getElementById("p1Score");
 const p2ScoreEl = document.getElementById("p2Score");
 const pauseBtn = document.getElementById("pauseBtn");
