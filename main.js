73162795a0189a48b178dbf2b6acd3584e3c9f1d
iancorbett <iancorbett1324@gmail.com>
2025-08-19 23:02:30 -0700

    define all necessary consts for buttons, scores, etc

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("game");
+const ctx = cvs.getContext("2d");
+const p1ScoreEl = document.getElementById("p1Score");
+const p2ScoreEl = document.getElementById("p2Score");
+const pauseBtn = document.getElementById("pauseBtn");
+const restartBtn = document.getElementById("restartBtn");
+const twoPlayerEl = document.getElementById("twoPlayer");
